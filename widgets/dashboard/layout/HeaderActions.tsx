72866db7a73b3c5a9c4ddcebcd516246a4910f24
mahdi.khoshghadam <mahdikgz7@gmail.com>
2026-03-21 00:11:37 +0330

    chore: mobile menu instead of sidebar

--- widgets/dashboard/layout/HeaderActions.tsx
+++ widgets/dashboard/layout/HeaderActions.tsx
@@ -1,15 +1,24 @@
 "use client";
 import React, { useState } from "react";
 import { Button, Drawer, Popconfirm } from "antd";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearUserProfile } from "@/entities/User/slices/Profile.slice";
 import { toast } from "react-toastify";
 import { RiLogoutBoxRLine, RiMenu2Line } from "react-icons/ri";
+import Link from "next/link";
+import { RootState } from "@/shared/configs/store";
+import { usePathname } from "next/navigation";
+import { adminSidebar, guestSidebar } from "@/constants/sidebar.constant";
 
 function HeaderActions() {
   const [open, setOpen] = useState<boolean>(false);
 
   const dispatch = useDispatch();
+
+  const { role } = useSelector((state: RootState) => state.profile);
+  const pathname = usePathname();
+
+  const sidebarData = role === "ADMIN" ? adminSidebar : guestSidebar;
 
   const handleConfirmLogout = () => {
     dispatch(clearUserProfile());
@@ -44,7 +53,30 @@ function HeaderActions() {
         onClose={() => setOpen(() => false)}
         size={256}
         title="دسترسی سریع"
-      />
+      >
+        <ul className="w-full flex flex-col items-start gap-3">
+          {sidebarData.map((link) => {
+            const isActive = pathname === link.url;
+            return (
+              <Link key={link.url} href={link.url} className="w-full">
+                <li
+                  className={`
+                          w-full py-3 px-2.5 flex items-center gap-2.5 rounded text-sm transition-all duration-300 font-medium
+                          ${
+                            isActive
+                              ? "bg-sky-600 text-white shadow-md"
+                              : "hover:bg-sky-600 hover:text-white text-gray-700"
+                          }
+                        `}
+                >
+                  {link.icon}
+                  {link.label}
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </Drawer>
     </div>
   );
 }
